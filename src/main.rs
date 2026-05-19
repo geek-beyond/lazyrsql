4ec7113c61c8ac4cf362f6d15938c68b44fa1b17
kouphasi <89081817+kouphasi@users.noreply.github.com>
2026-05-20 00:53:02 +0900

    tuiの環境構築 (#17)
    * feat: setup rust
    
    * chore: add ratatui
    
    * add: sample ratatui
    
    * add: typosのホワイトリストにratatuiを追加
    
    * Revert "add: sample ratatui"
    
    This reverts commit efb6766f5844c00732cf7cd876ef365c1ad8d5ba.
    
    * feat: add hello world
    
    * feat: エラーが発生する部分でエラーを移譲するように
    
    * refactor: indentの調整
    
    * feat: qを推したら終了するように
    
    * add: typos cli
    
    * chore: typosのバージョンを固定
    
    * feat: main関数の戻り値を()に
    
    * feat: エラーハンドリングの追加
    
    * refactor: main関数をシンプルに
    
    * remove: color_eyre

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use ratatui::{DefaultTerminal, Frame};
+
+fn main() -> std::io::Result<()> {
+    ratatui::run(app)
+}
+
+fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    let key_event_q = KeyEvent::new(KeyCode::Char('q'), KeyModifiers::NONE);
+    loop {
+        terminal.draw(render)?;
+        if crossterm::event::read()?.as_key_press_event() == Some(key_event_q) {
+            break Ok(());
+        }
+        }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
+}
